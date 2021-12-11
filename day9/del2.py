42ca30c68a9e44926792a1a2cf04449fdaf9afb9
Erik Snartland <erik.snartland@skatteetaten.no>
2021-12-11 16:30:05 +0100

    day9 part 2

--- day9/del2.py
+++ day9/del2.py
@@ -2,14 +2,21 @@
 
 import numpy as np
 import pandas as pd
+import sys
+sys.setrecursionlimit(10000)
 
-class Position:
-    def __init__(self):
-        self.number = 99
-        self.up = 99
-        self.down = 99
-        self.left = 99
-        self.right = 99
+def neighbors(df, basin, i, j, basinNumber):
+
+    if i < df.shape[0] and j < df.shape[1] and i >= 0 and j >= 0:
+        if int(df.iloc[i][j]) < 9:
+            if int(basin.iloc[i][j]) == basinNumber:
+                return basin
+            basin[j][i] = basinNumber
+            basin = neighbors(df, basin, i, j+1, basinNumber)
+            basin = neighbors(df, basin, i+1, j, basinNumber)
+            basin = neighbors(df, basin, i, j-1, basinNumber)
+            basin = neighbors(df, basin, i-1, j, basinNumber)
+    return basin
 
 def split(word):
     return [char for char in word]
@@ -20,34 +27,51 @@ def readFile():
 
 def main():
     heightmap = readFile()
-    print(heightmap)
-    positions = []
-    lowPoints = []
     sum = []
     heightmap = np.array([split(x) for x in heightmap.ravel()])
     df = pd.DataFrame(heightmap)
-    print(df)
-    for i, row in df.iterrows():
-        for j, element in enumerate(row):
-            p = Position()
-            p.number = int(df.iloc[i][j])
-            if i-1 >= 0 and i-1 < df.shape[0]:
-                p.up = int(df.iloc[i-1][j])
-            if i+1 >= 0 and i+1 < df.shape[0]:
-                p.down = int(df.iloc[i+1][j])
-            if j+1 >= 0 and j+1 < df.shape[1]:
-                p.right = int(df.iloc[i][j+1])
-            if j-1 >= 0 and j-1 < df.shape[1]:
-                p.left = int(df.iloc[i][j-1])
-            positions.append(p)
-    
-    risk = 0
-    for p in positions:
-        if p.number < p.up and p.number < p.down and p.number < p.left and p.number < p.right:
-            print(p.number)
-            risk = risk + p.number + 1
-    print(risk)
-    
+    basin = pd.DataFrame(np.zeros((df.shape[0], df.shape[1])))
+    basinNumber = 1
+
+    i = 0
+    j = 0
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            if int(df.iloc[i][j]) < 9:
+                if basinNumber == 1:
+                    basin = neighbors(df, basin, i, j, basinNumber)                
+                    basinNumber += 1
+                elif int(basin.iloc[i][j]) == 0.0:
+                    basin = neighbors(df, basin, i, j, basinNumber)
+                    basinNumber += 1
+            j += 1
+        i += 1
+    print(basin)
+    max_value = 0
+    for row in basin:
+        if max_value < basin[row].max():
+            max_value = basin[row].max()
+
+    print(max_value)
+
+    dicts = {}
+    keys = range(int(max_value))
+    for row in basin:
+        print((basin[row] == 3).sum())
+        
+        for i in keys:
+            dicts[i] = dicts.get(i, 0) + (basin[row] == i+1).sum()
+    print(dicts)
+    my_keys = sorted(dicts, key=dicts.get, reverse=True)[:3]
+    print(my_keys)
+    mult = []
+    for key in my_keys:
+        mult.append(dicts[key])
+
+    result = np.prod(mult)
+    print(result)
+
+
 # Control execution of code
 if __name__ == "__main__":
     main()
